f4058347b871a060892c347e03b6c13548614d8b
KimSeolHee <tjfgml64@nate.com>
2022-07-27 12:22:35 +0900

    Test

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -6,11 +6,9 @@ import com.iu.util.DBConnector;
 
 public class TestMain {
 
-	public static void main(String[] args) {
-		DBConnector dbConnector = new DBConnector();
-		
+	public static void main(String[] args) {	
 		try {
-			dbConnector.getConnection();
+			DBConnector.getConnection();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
